0263b4ce8758fdd941b976a6e597449c52c8958c
Carlin Bingham <cb@viennan.net>
2018-02-05 00:58:11 +1300

    can enumerate windows and get workspace

--- winf.c
+++ winf.c
@@ -6,13 +6,12 @@
 
 
 int
-main()
+getcurrentworkspace()
 {
 	Display *dpy;
 	Atom a;
 	Window w;
 	int screen;
-	int status;
 
 	Atom typeret;
 	int fmtret;
@@ -31,7 +30,7 @@ main()
 
 	w = RootWindow(dpy, screen);
 
-	status = XGetWindowProperty(dpy, w, a, 0, 0x7fffffff, False,
+	XGetWindowProperty(dpy, w, a, 0, 0x7fffffff, False,
 			XA_CARDINAL, &typeret, &fmtret, &numret,
 			&bytesafter, &d);
 
@@ -43,4 +42,60 @@ main()
 
 	XFree(d);
 	XCloseDisplay(dpy);
+}
+
+int
+main()
+{
+	Display *dpy;
+	Atom a;
+	Window w;
+	int screen;
+	unsigned long i;
+
+	Atom typeret;
+	int fmtret;
+	unsigned long bytesafter, numret;
+	Window *d;
+
+	long *p;
+
+	if ((dpy = XOpenDisplay(":0")) == NULL) {
+		err(1, "XOpenDisplay");
+	}
+
+	screen = XDefaultScreen(dpy);
+
+	if ((a = XInternAtom(dpy, "_NET_CLIENT_LIST", True)) == None) {
+		err(1, "XInternAtom");
+	}
+
+	w = RootWindow(dpy, screen);
+
+	XGetWindowProperty(dpy, w, a, 0, 0x7fffffff, False,
+			XA_WINDOW, &typeret, &fmtret, &numret,
+			&bytesafter, (unsigned char **)&d);
+
+	if (numret == 0) {
+		err(1, "blah");
+	}
+
+	if ((a = XInternAtom(dpy, "_NET_WM_DESKTOP", True)) == None) {
+		err(1, "XInternAtom");
+	}
+
+	for (i = 0; i < numret; i++) {
+		unsigned long *l;
+		Atom wtyperet;
+		int wfmtret;
+		unsigned long wbytesafter, wnumret;
+
+		XGetWindowProperty(dpy, d[i], a, 0, 0x7fffffff, False,
+				XA_CARDINAL, &wtyperet, &wfmtret, &wnumret,
+				&wbytesafter, (unsigned char **)&l);
+		printf("%lu\n", *l);
+		XFree(l);
+	}
+
+	return 0;
 }
