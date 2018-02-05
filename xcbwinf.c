aaf61472363fcdde474438e801051f62f0bac24e
Carlin Bingham <cb@viennan.net>
2018-02-06 01:16:20 +1300

    getting workspaces from windows works

--- xcbwinf.c
+++ xcbwinf.c
@@ -44,10 +44,12 @@ getcurrentdesktop()
 	printf("%i\n", *l);
 
 	xcb_disconnect(conn);
+
+	return 0;
 }
 
-int
-getwindowlist()
+xcb_window_t *
+getwindowlist(uint32_t *sz)
 {
 	xcb_connection_t *conn;
 	xcb_screen_t *scr;
@@ -81,5 +83,62 @@ getwindowlist()
 		printf("%i\n", list[i]);
 	}
 
+	*sz = prorep->length;
+
 	xcb_disconnect(conn);
+
+	return list;
+}
+
+int
+getactiveworkspaces()
+{
+	xcb_connection_t *conn;
+	xcb_screen_t *scr;
+	xcb_intern_atom_cookie_t atcook;
+	xcb_intern_atom_reply_t *rep;
+
+	xcb_get_property_cookie_t procook;
+	xcb_get_property_reply_t *prorep;
+
+	xcb_window_t *list;
+	uint32_t sz;
+	uint32_t i;
+	int *ws;
+
+	const char ncd_name[] = "_NET_WM_DESKTOP";
+
+	list = getwindowlist(&sz);
+
+	if ((conn = xcb_connect(NULL, NULL)) == NULL)
+		err(1, "xcb_connect");
+	scr = xcb_aux_get_screen(conn, 0);
+
+	atcook = xcb_intern_atom(conn, 1, strlen(ncd_name), ncd_name);
+	if ((rep = xcb_intern_atom_reply(conn, atcook, NULL)) == 0)
+		err(1, "xcb_intern_atom_reply");
+
+	for (i = 0; i < sz; i++) {
+		procook = xcb_get_property(conn, 0, list[i],
+				rep->atom, XCB_ATOM_CARDINAL, 0, 4096);
+		if ((prorep = xcb_get_property_reply(conn, procook, NULL)) == NULL)
+		err(1, "xcb_get_property_reply");
+
+		if (prorep->length == 0)
+			err(1, "xcb_get_property_reply length = 0");
+
+		ws = (int *)xcb_get_property_value(prorep);
+
+		printf("%i\n", *ws);
+	}
+
+	xcb_disconnect(conn);
+
+	return 0;
+}
+
+int
+main()
+{
+	getactiveworkspaces();
 }
